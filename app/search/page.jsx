30397db5db86f88d10d4d7623053b605814f3a4a
ADecentName <iamanoobirl@gmail.com>
2025-02-08 03:33:03 +0800

    Updated css for search results

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -8,45 +8,45 @@ const API_KEY = '51372fec0f0d192195fa00d7602b7900';
 const SearchPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const query = searchParams.get('query'); // Get the search query from URL
+  const query = searchParams.get('query');
   const [results, setResults] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);  // Loading state
+  const [isLoading, setIsLoading] = useState(false);  
 
-  // Effect to fetch search results based on query and currentPage
+
   useEffect(() => {
     if (query) {
-      setIsLoading(true);  // Start loading
+      setIsLoading(true);  
       fetch(`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}&page=${currentPage}`)
         .then((res) => res.json())
         .then((data) => {
-          setResults(currentPage === 1 ? data.results : [...results, ...data.results]); // Reset if it's the first page
+          setResults(currentPage === 1 ? data.results : [...results, ...data.results]); 
           setTotalPages(data.total_pages);
-          setIsLoading(false);  // Stop loading after data is fetched
+          setIsLoading(false);  
         })
         .catch((error) => {
           console.error('Error fetching data:', error);
-          setIsLoading(false);  // Stop loading on error
+          setIsLoading(false);  
         });
     }
-  }, [query, currentPage]); // Trigger when query or currentPage changes
+  }, [query, currentPage]); 
 
   const loadMoreResults = () => {
     if (currentPage < totalPages) {
-      setCurrentPage((prevPage) => prevPage + 1); // Increment page number
+      setCurrentPage((prevPage) => prevPage + 1);
     }
   };
 
   return (
     <div className="popular-movies">
       <h2 className="popular-movies">Search Results for "{query}"</h2>
-      <div className="search-results-grid"> {/* Updated class for grid layout */}
+      <div className="search-results-grid"> 
         {isLoading ? (
           <p>Loading...</p>
         ) : results.length > 0 ? (
           results.map((item) => (
-            <div key={item.id} className="search-card"> {/* Updated class for each movie card */}
+            <div key={item.id} className="search-card"> 
               <img
                 src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
                 alt={item.title || item.name}
@@ -60,7 +60,6 @@ const SearchPage = () => {
           <p>No results found.</p>
         )}
       </div>
-      {/* Only show the "Load More" button if there are more pages to load */}
       {currentPage < totalPages && !isLoading && (
         <button onClick={loadMoreResults}>Load More</button>
       )}
